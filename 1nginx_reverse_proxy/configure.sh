ea40b73d77a7746ba8a9dc49b0a56e1a29b488e5
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-08 18:06:23 +0530

    Add: added the README + updated the scripts

--- 1nginx_reverse_proxy/configure.sh
+++ 1nginx_reverse_proxy/configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo docker cp /home/psychopunk_sage/Code/Lang/API/django/doc_vivenns/default.conf proxy:/etc/nginx/conf.d/
-sudo docker exec proxy nginx -t
+docker cp {UPDATED default.conf PARENT location}/default.conf proxy:/etc/nginx/conf.d/
+docker exec proxy nginx -t
 echo "Re-loading..."
-sudo docker exec proxy nginx -s reload
+docker exec proxy nginx -s reload
 echo "Re-starting..."
-sudo service nginx restart
+service nginx restart
