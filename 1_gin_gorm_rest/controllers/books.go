56018cfbadd787befb2b10e7b3ec53d5ad3f46b1
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-14 16:17:23 +0530

    feat: CreateBook <controller> Done.

--- 1_gin_gorm_rest/controllers/books.go
+++ 1_gin_gorm_rest/controllers/books.go
@@ -7,6 +7,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
 // return all books from our database
 func FindBooks(ctx *gin.Context) {
 	var books []models.Book
@@ -15,4 +20,23 @@ func FindBooks(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"BookData": books,
 	})
+}
+
+func CreateBook(ctx *gin.Context) {
+	// 1. Create an input (type: CreateBookInput struct)
+	var input CreateBookInput
+
+	err := ctx.BindJSON(&input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// 2. Create a new book
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	ctx.JSON(http.StatusOK, gin.H{"data": book})
 }
