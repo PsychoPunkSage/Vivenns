69f76d5c6f682a2e8390edc81f8603f8e3e98e0f
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-15 12:31:59 +0530

    delete: 1nginx_reverse_proxy

--- 1nginx_reverse_proxy/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker cp {UPDATED default.conf PARENT location}/default.conf proxy:/etc/nginx/conf.d/
-docker exec proxy nginx -t
-echo "Re-loading..."
-docker exec proxy nginx -s reload
-echo "Re-starting..."
-service nginx restart
