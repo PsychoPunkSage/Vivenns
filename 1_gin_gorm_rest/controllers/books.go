cf9b0cf4d10519426c724e13af667f4505b06e57
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-15 13:10:58 +0530

    fix: CreateBook <controller> re-written

--- 1_gin_gorm_rest/controllers/books.go
+++ 1_gin_gorm_rest/controllers/books.go
@@ -63,34 +63,23 @@ func FindBook(c *gin.Context) { // Get model if exist
 // //////////////
 // CREATE_BOOK //
 // //////////////
-func CreateBook(c *gin.Context) {
-	// // 1. Create an input (type: CreateBookInput struct)
-	// var input CreateBookInput
-
-	// err := ctx.BindJSON(&input)
-	// if err != nil {
-	// 	ctx.JSON(http.StatusBadRequest, gin.H{
-	// 		"error": err.Error(),
-	// 	})
-	// 	return
-	// }
-
-	// // 2. Create a new book
-	// book := models.Book{Title: input.Title, Author: input.Author}
-	// models.DB.Create(&book)
-
-	// ctx.JSON(http.StatusOK, gin.H{"data": book})
+func CreateBook(ctx *gin.Context) {
+	// 1. Create an input (type: CreateBookInput struct)
 	var input CreateBookInput
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+	err := ctx.BindJSON(&input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
 		return
 	}
 
-	// Create book
+	// 2. Create a new book
 	book := models.Book{Title: input.Title, Author: input.Author}
 	models.DB.Create(&book)
 
-	c.JSON(http.StatusOK, gin.H{"data": book})
+	ctx.JSON(http.StatusOK, gin.H{"data": book})
 }
 
 // //////////////
@@ -142,99 +131,3 @@ func DeleteBook(ctx *gin.Context) {
 		"Delete-Status": true,
 	})
 }
-
-// // DELETE /books/:id
-// // Delete a book
-// func DeleteBook(c *gin.Context) {
-// 	// Get model if exist
-// 	var book models.Book
-// 	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-// 		return
-// 	}
-
-// package controllers
-
-// import (
-// 	"net/http"
-
-// 	"1_gin_gorm_rest/models"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// type CreateBookInput struct {
-// 	Title  string `json:"title" binding:"required"`
-// 	Author string `json:"author" binding:"required"`
-// }
-
-// type UpdateBookInput struct {
-// 	Title  string `json:"title"`
-// 	Author string `json:"author"`
-// }
-
-// // GET /books
-// // Find all books
-// func FindBooks(c *gin.Context) {
-// 	var books []models.Book
-// 	models.DB.Find(&books)
-
-// 	c.JSON(http.StatusOK, gin.H{"data": books})
-// }
-
-// // GET /books/:id
-// // Find a book
-// func FindBook(c *gin.Context) {
-// 	// Get model if exist
-// 	var book models.Book
-// 	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"data": book})
-// }
-
-// // POST /books
-// // Create new book
-// func CreateBook(c *gin.Context) {
-// 	// Validate input
-// 	var input CreateBookInput
-// 	if err := c.ShouldBindJSON(&input); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	// Create book
-// 	book := models.Book{Title: input.Title, Author: input.Author}
-// 	models.DB.Create(&book)
-
-// 	c.JSON(http.StatusOK, gin.H{"data": book})
-// }
-
-// // PATCH /books/:id
-// // Update a book
-// func UpdateBook(c *gin.Context) {
-// 	// Get model if exist
-// 	var book models.Book
-// 	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-// 		return
-// 	}
-
-// 	// Validate input
-// 	var input UpdateBookInput
-// 	if err := c.ShouldBindJSON(&input); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	models.DB.Model(&book).Updates(input)
-
-// 	c.JSON(http.StatusOK, gin.H{"data": book})
-// }
-
-// 	models.DB.Delete(&book)
-
-// 	c.JSON(http.StatusOK, gin.H{"data": true})
-// }
