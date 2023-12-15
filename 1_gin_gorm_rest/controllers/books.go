83609f0cb7caccafabd35e884540b11db4ee4ed1
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-15 13:40:29 +0530

    fix: Documented Controllers

--- 1_gin_gorm_rest/controllers/books.go
+++ 1_gin_gorm_rest/controllers/books.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// `....Text inside....` :: For Serialization + Deserialization || AS Go doesn't support JSON
 type CreateBookInput struct {
 	Title  string `json:"title" binding:"required"`
 	Author string `json:"author" binding:"required"`
@@ -22,9 +23,13 @@ type UpdateBookInput struct {
 // /////////////
 // return all books from our database
 func FindBooks(ctx *gin.Context) {
+	// <<books>> empty slice to hold the list of books
 	var books []models.Book
+
+	// Use the DB connection to retrieve all Book models and store them in <<books>>
 	models.DB.Find(&books)
 
+	// Return response as JSON with HTTP 200 OK status + books data (as JSON)
 	ctx.JSON(http.StatusOK, gin.H{
 		"BooksData": books,
 	})
@@ -36,8 +41,15 @@ func FindBooks(ctx *gin.Context) {
 
 // return particular book from our database
 func FindBook(ctx *gin.Context) {
+	// <<book>> var. to hold the "Book" struct i.e. for single book
 	var book models.Book
 
+	// Fetch the book with ID
+	/*
+		<<ctx.Param("id")>> retrieves the "id" parameter that was passed in the API URL.
+		<<.First(&book)>> retrieve the FIRST matching record, and it will be loaded into the book struct
+		<<Error>> executes the SQL query and returns any error encountered
+	*/
 	err := models.DB.Where("id = ?", ctx.Param("id")).First(&book).Error
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
@@ -45,6 +57,7 @@ func FindBook(ctx *gin.Context) {
 		})
 	}
 
+	// Return response as JSON with HTTP 200 OK status + book data (as JSON)
 	ctx.JSON(http.StatusOK, gin.H{
 		"BookData": book,
 	})
@@ -57,6 +70,7 @@ func CreateBook(ctx *gin.Context) {
 	// 1. Create an input (type: CreateBookInput struct)
 	var input CreateBookInput
 
+	// 1a. Bind the JSON body data to input struct || fails will be recorded in `err`
 	err := ctx.BindJSON(&input)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
@@ -67,8 +81,11 @@ func CreateBook(ctx *gin.Context) {
 
 	// 2. Create a new book
 	book := models.Book{Title: input.Title, Author: input.Author}
+
+	// 3. Insert the new book record using the GORM DB connection
 	models.DB.Create(&book)
 
+	// 4. Return response as JSON with HTTP 200 OK status + book data (as JSON)
 	ctx.JSON(http.StatusOK, gin.H{"data": book})
 }
 
@@ -76,8 +93,15 @@ func CreateBook(ctx *gin.Context) {
 // UPDATE_BOOK //
 // //////////////
 func UpdateBook(ctx *gin.Context) {
-	// Find the Book
+	// <<book>> var. to hold the "Book" struct i.e. for single book
 	var book models.Book
+
+	// Fetch the book with ID
+	/*
+		<<ctx.Param("id")>> retrieves the "id" parameter that was passed in the API URL.
+		<<.First(&book)>> retrieve the FIRST matching record, and it will be loaded into the book struct
+		<<Error>> executes the SQL query and returns any error encountered
+	*/
 	err := models.DB.Where("id = ?", ctx.Param("id")).First(&book).Error
 
 	if err != nil {
@@ -96,6 +120,7 @@ func UpdateBook(ctx *gin.Context) {
 	// Update the input
 	models.DB.Model(&book).Updates(input)
 
+	// Return response as JSON with HTTP 200 OK status + updatedBook data (as JSON)
 	ctx.JSON(http.StatusOK, gin.H{
 		"book": book,
 	})
@@ -105,9 +130,17 @@ func UpdateBook(ctx *gin.Context) {
 // DELETE_BOOK //
 // //////////////
 func DeleteBook(ctx *gin.Context) {
-	// Find the book
+	// <<book>> var. to hold the "Book" struct i.e. for single book
 	var book models.Book
+
+	// Fetch the book with ID
+	/*
+		<<ctx.Param("id")>> retrieves the "id" parameter that was passed in the API URL.
+		<<.First(&book)>> retrieve the FIRST matching record, and it will be loaded into the book struct
+		<<Error>> executes the SQL query and returns any error encountered
+	*/
 	err := models.DB.Where("id =  ?", ctx.Param("id")).First(&book).Error
+
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"Error":   "Books Does not exist",
@@ -117,6 +150,8 @@ func DeleteBook(ctx *gin.Context) {
 
 	// Delete book
 	models.DB.Delete(&book)
+
+	// Return response as JSON with HTTP 200 OK status + Status
 	ctx.JSON(http.StatusOK, gin.H{
 		"Delete-Status": true,
 	})
