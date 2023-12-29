95e36df37e76c692316cdda957345a8a454cbc4d
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-29 15:42:20 +0530

    feat: completed script

--- 6_private_data_collections/network_private_data.sh
+++ 6_private_data_collections/network_private_data.sh
@@ -1,12 +1,17 @@
-
-echo "Start the network"
+echo "============================================================="
+echo "|||||||||||||||||||  NETWORK IS STARTING  |||||||||||||||||||"
+echo "============================================================="
 ./network.sh down
 ./network.sh up createChannel -ca -s couchdb
 
-echo "Deploy the private data smart contract to the channel"
+echo "============================================================="
+echo "||  DEPLOY THE PRIVATE DATA SAMRT CONTRACT TO THE CHANNEL  ||"
+echo "============================================================="
 ./network.sh deployCC -ccn private -ccp ../asset-transfer-private-data/chaincode-go/ -ccl go -ccep "OR('Org1MSP.peer','Org2MSP.peer')" -cccg ../asset-transfer-private-data/chaincode-go/collections_config.json
 
-echo "Register identities"
+echo "============================================================="
+echo "|||||||||||||||||||  REGISTER IDENTITIES  |||||||||||||||||||"
+echo "============================================================="
 export PATH=${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 
@@ -34,18 +39,6 @@ fabric-ca-client enroll -u https://buyer:buyerpw@localhost:8054 --caname ca-org2
 echo "copy the Node OU configuration file into the buyer identity MSP folder."
 cp "${PWD}/organizations/peerOrganizations/org2.example.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/org2.example.com/users/buyer@org2.example.com/msp/config.yaml"
 
-echo "Create an asset in private data"
-
-echo ""
-export PATH=${PWD}/../bin:$PATH
-export FABRIC_CFG_PATH=$PWD/../config/
-export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/owner@org1.example.com/msp
-export CORE_PEER_ADDRESS=localhost:7051
-
-echo "Create Asset"
-export ASSET_PROPERTIES=$(echo -n "{\"objectType\":\"asset\",\"assetID\":\"asset1\",\"color\":\"green\",\"size\":20,\"appraisedValue\":100}" | base64 | tr -d \\n)
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n private -c '{"function":"CreateAsset","Args":[]}' --transient "{\"asset_properties\":\"$ASSET_PROPERTIES\"}"
-
+echo "============================================================"
+echo "<<<<<<<<<<<<<  CREATE AN ASSET IN PRIVATE DATA  >>>>>>>>>>>>"
+echo "============================================================"
