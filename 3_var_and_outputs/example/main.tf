a1c8c0a8375b37167cd48a770a3925606e6d8614
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-02-04 09:11:01 +0530

    fix: added 3_var_and_outputs > example DONE......

--- 3_var_and_outputs/example/main.tf
+++ 3_var_and_outputs/example/main.tf
@@ -32,3 +32,14 @@ resource "aws_instance" "instance" {
     ExtrraTag = local.extra_tag
   }
 }
+
+resource "aws_db_instance" "db_instance" {
+  allocated_storage   = 20
+  storage_type        = "gp2"
+  engine              = "postgres"
+  engine_version      = "12.4"
+  instance_class      = "db.t2.micro"
+  username            = var.db_user
+  password            = var.db_pass
+  skip_final_snapshot = true
+}
---
+++ 3_var_and_outputs/example/terraform.tfvars
@@ -0,0 +1,6 @@
+# Non-sensitive infos are here
+
+# Bydefault << .tfvars >> file 
+instance_name = "hello-psychopunksage"
+instance_type = "t2.micro"
+ami           = "ami-011899242bb902164"
